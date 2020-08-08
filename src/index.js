bdd96dbcc50ad79acaf514dc5f08100b90405714
mohesem <mohesem@gmail.com>
2020-08-08 12:58:17 +0000

    first

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import Team from './models/teamsModel'
+import Team from "./models/teamsModel";
 
 mongoose.connect("mongodb://localhost:27017/fansclub", {
   useUnifiedTopology: true,
@@ -8,11 +8,9 @@ mongoose.connect("mongodb://localhost:27017/fansclub", {
 });
 
 mongoose.connection.on("connected", () => {
-  log("MongoDB connected");
-    (async () => {
-
-        const count = await Team.countDocuments({})
-        console.log(count)
-
-    })
+  console.log("MongoDB connected");
+  async () => {
+    const count = await Team.countDocuments({});
+    console.log(count);
+  };
 });
