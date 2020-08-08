1d5b054da4df7e77d0c572682c5eef148a71743a
mohesem <mohesem@gmail.com>
2020-08-08 12:45:28 +0000

    first

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import Team from './models/teamsModel'
 
 mongoose.connect("mongodb://localhost:27017/fansclub", {
   useUnifiedTopology: true,
@@ -8,4 +9,10 @@ mongoose.connect("mongodb://localhost:27017/fansclub", {
 
 mongoose.connection.on("connected", () => {
   log("MongoDB connected");
+    (async () => {
+
+        const count = await Team.countDocuments({})
+        console.log(count)
+        
+    })
 });
